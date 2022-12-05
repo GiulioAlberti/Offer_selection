e03be717821b2e09616dd9ecd2000470bf05956a
GiulioAlberti <g3alberti@gmail.com>
2022-12-05 16:51:28 +0100

    Better params

--- input.py
+++ input.py
@@ -1,24 +1,20 @@
 import numpy as np
 
-np.random.seed(160)
-num_flights = 60
-interval = 3
+np.random.seed(24)
+num_flights = 45
+interval = 1
 interval_modifier = 2
 total_int = interval * interval_modifier
-# cost_coefficients = np.array(np.random.randint(1, 5, num_flights))     era per il caso quadratico
-
-slopes_vect = np.array(1 / np.random.randint(2, 7, num_flights))
-margins_vect = np.array(
-    [(np.random.randint((num_flights - x) * total_int // 8, 3 * (num_flights - x) * total_int // 4)) for x in
-     range(num_flights)])
-jumps_vect = np.array(np.random.randint(10, 50, num_flights))
-
-cost_coefficients = np.array([(slopes_vect[fl], margins_vect[fl], jumps_vect[fl]) for fl in range(num_flights)])
-
-airlines_name_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
-# airlines_name_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]
-airlines_name = np.random.choice(airlines_name_list, num_flights)
-
 indexes = range(num_flights)
 slots = np.array([interval * index for index in indexes])
 new_slot_times = np.array([interval_modifier * slot for slot in slots])
+
+slopes_vect = np.array(np.random.uniform(0, 1 / 6, num_flights))
+margins_vect = np.array([(np.random.uniform(15, max([20, new_slot_times[-1] + 5 - x]))) for x in slots])
+jumps_vect = np.array(np.random.uniform(5, 20, num_flights))
+
+cost_coefficients = np.array([(slopes_vect[fl], margins_vect[fl], jumps_vect[fl]) for fl in range(num_flights)])
+
+airlines_name_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+# airlines_name_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]
+airlines_name = np.random.choice(airlines_name_list, num_flights)
